f3756109a2ec7f2be1b68d68102736b67ca6cc1f
franciscocfreire@gmail.com <franciscocfreire@gmail.com>
2019-04-27 11:09:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { PlacarComponent } from './placar/placar.component';
+import { ClubesComponent } from './clubes/clubes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    PlacarComponent,
+    ClubesComponent
   ],
   imports: [
     BrowserModule
