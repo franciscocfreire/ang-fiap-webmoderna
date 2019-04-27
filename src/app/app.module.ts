9da54ced0b242bfb2e3ec5b7e0dea46e406e416b
franciscocfreire@gmail.com <franciscocfreire@gmail.com>
2019-04-27 11:56:04 -0300

    Master Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HerosComponent } from './heros/heros.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { PlacarComponent } from './placar/placar.component';
 import { ClubesComponent } from './clubes/clubes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HerosComponent,
+    HeroesComponent,
     PlacarComponent,
-    ClubesComponent
+    ClubesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
